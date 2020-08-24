48e6b19b15cb2c01287c942a5da905a0564607a9
Datuluna Dilangalen <dgdilangalen@up.edu.ph>
2020-08-24 14:04:32 +0800

    added part to try to parsing whole _source JSON object

--- routes/array.js
+++ routes/array.js
@@ -1,28 +1,29 @@
 
-var express = require('express');
-var router = express.Router();
-router.use(express.json());
+var express = require('express')
+var router = express.Router()
+router.use(express.json())
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('You\'ve come to the array');
-});
+router.get('/', function (req, res, next) {
+  res.send('You\'ve come to the array')
+})
 
-router.post('/', function(req, res, next) {
-  arr = req.body;
-  console.log(arr);
-  json_resp = {};
+router.post('/', function (req, res, next) {
+  arr = req.body
+  console.log(arr)
+  json_resp = {}
   url_arr = []
   for (i = 0; i < arr.hits.length; i++) {
-    url = arr.hits[i]._source.url.full;
-    ip = arr.hits[i]._source.destination.ip;
-    b = new Buffer(url);
-    urlb64 = (b.toString('base64')).replace(/=/g, '');
-    url_arr.push({'url': url, 'ip': ip, 'urlb64': urlb64});
+    full_source = arr.hits[i]._source
+    url = arr.hits[i]._source.url.full
+    ip = arr.hits[i]._source.destination.ip
+    b = new Buffer(url)
+    urlb64 = (b.toString('base64')).replace(/=/g, '')
+    url_arr.push({ url: url, ip: ip, urlb64: urlb64 })
   };
-  json_resp['array'] = url_arr;
-  json_resp['length'] = arr.hits.length;
-  res.type('json').send(json_resp);
-});
+  json_resp.array = url_arr
+  json_resp.length = arr.hits.length
+  res.type('json').send(json_resp)
+})
 
-module.exports = router;
+module.exports = router
