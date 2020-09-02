ff6cbabcca4a1974f8b92210a2f6db86a39c8c1d
Luna Dilangalen <dadilangalen@gmail.com>
2020-09-02 17:09:19 +0800

    initialized threat intel api route

---
+++ routes/threat_intel.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router()
+router.use(express.json())
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.send('You\'ve come to the threat intel api')
+})
+
+module.exports = router
