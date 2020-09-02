86544064c56c016e1b948d953fe904348e21a8aa
Luna Dilangalen <dadilangalen@gmail.com>
2020-09-02 17:30:49 +0800

    added POST request to threat intel API

--- routes/threat_intel.js
+++ routes/threat_intel.js
@@ -7,4 +7,14 @@ router.get('/', function (req, res, next) {
   res.send('You\'ve come to the threat intel api')
 })
 
+router.post('/virustotal/ip', function(req, res, next) {
+    body = req.body;
+    console.log(body);
+    const response = {
+        message: 'Should be some IP'
+    }
+    res.type('json').send(response)
+
+})
+
 module.exports = router
