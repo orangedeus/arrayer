637824da957b1a07325037cfcb092cfcb4968ee3
Luna Dilangalen <dadilangalen@gmail.com>
2020-09-03 14:10:32 +0800

    created promise loop

--- routes/threat_intel.js
+++ routes/threat_intel.js
@@ -13,56 +13,49 @@ router.get('/', function (req, res, next) {
   res.send('You\'ve come to the threat intel api')
 })
 
-router.post('/virustotal', async function (req, res, next) {
+router.post('/virustotal', function (req, res, next) {
   data = req.body;
   console.log(data);
-  var report, vote
-  var reports = [];
-  var votes = [];
+  var reports = ''
   // iterate through list of _source
-  for (let i=0; i < data.length; i++) {
-    // get datetime 
-    var dt = dateTime.create();
-    var formatted_date = dt.format('Y-m-d-H-M-S');
-    console.log('datetime: ' + formatted_date);
+  const loop = async function (data, reports) {
+    for (let i = 0; i < data.length; i++) {
+      // get datetime 
+      var dt = dateTime.create();
+      var formatted_date = dt.format('Y-m-d-H-M-S');
+      console.log('datetime: ' + formatted_date);
 
-    // parse data
-    var source_ip = data[i].source.ip
-    var dns_question_name = data[i].dns.question.name
-    var answers_count = data[i].dns.answers_count
-    var resolved_ip = data[i].dns.resolved_ip 
+      // parse data
+      var source_ip = data[i].source.ip
+      var dns_question_name = data[i].dns.question.name
+      var answers_count = data[i].dns.answers_count
+      var resolved_ip = data[i].dns.resolved_ip
 
-    //console.log(response.data);
-    
-    report = "-----------------------------------------------------------\n" +
-      "source: " + source_ip + "\n" +
-      "question: " + dns_question_name + "\n"
-    reports.push(report)
+      // report generation
+      var report = "-----------------------------------------------------------\n" +
+                   "source: " + source_ip + "\n" +
+                   "question: " + dns_question_name + "\n"
 
-    // TODO: query threatcrowd API
-    axios
-      .get('https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=' + dns_question_name)
-      .then(response => {
-        // console.log(response.data.votes);
-        vote = "threatcrowd votes: " + response.data.votes + "\n"
-        votes.push(vote)
-      })
-      .catch(error => {
-        console.log(error);
+      // query threatcrowd API
+      var response = await axios.get('https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=' + dns_question_name)
+      report = report + response.data.votes
+
+      // aggregate all reports
+      reports = reports + report
+
+      fs.appendFile("./reports/" + formatted_date + ".txt", report, (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
+        // success case, the file was saved
+        console.log('Report saved!');
       });
-    
-    fs.appendFile("./reports/" + formatted_date + ".txt", report, (err) => {
-      // throws an error, you could also catch it here
-      if (err) throw err;
-      // success case, the file was saved
-      console.log('Report saved!');
-    });
+    }
   }
-  console.log(reports)
-  console.log(votes)
+
+  loop(data, reports)
 
   const response = {
-    message: 'See Reports at reports directory'
+    message: reports
   }
   res.type('json').send(response)
 
