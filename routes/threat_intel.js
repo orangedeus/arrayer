6719a74ef878e5b7078254f1eaaa6488fc17439e
Luna Dilangalen <dadilangalen@gmail.com>
2020-09-03 13:10:44 +0800

    moved report generation to promise

--- routes/threat_intel.js
+++ routes/threat_intel.js
@@ -20,33 +20,30 @@ router.post('/virustotal', function (req, res, next) {
   var votes = [];
   // iterate through list of _source
   for (let i=0; i < data.length; i++) {
-    // get datetime 
-    var dt = dateTime.create();
-    var formatted_date = dt.format('Y-m-d-H-M-S');
-    console.log('datetime: ' + formatted_date);
-
     // parse data
     var source_ip = data[i].source.ip
     var dns_question_name = data[i].dns.question.name
     var answers_count = data[i].dns.answers_count
-    if (data[i].dns.resolved_ip != undefined) {
-      var resolved_ip = 
-    }
+    var resolved_ip = data[i].dns.resolved_ip 
 
     // TODO: query threatcrowd API
     axios
       .get('https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=' + dns_question_name)
       .then(response => {
+        // get datetime 
+        var dt = dateTime.create();
+        var formatted_date = dt.format('Y-m-d-H-M-S');
+        console.log('datetime: ' + formatted_date);
+
         //console.log(response.data);
         console.log(response.data.votes);
         report = "-----------------------------------------------------------\n" +
              "source: " + source_ip + "\n" +
              "question: " + dns_question_name + "\n" +
              "threatcrowd votes: " + response.data.votes + "\n"
-        fs.writeFile('.txt', JSON.stringify(json_resp), (err) => {
+        fs.writeFile("./reports/" + formatted_date + ".txt", report, (err) => {
           // throws an error, you could also catch it here
           if (err) throw err;
-
           // success case, the file was saved
           console.log('Report saved!');
         });
