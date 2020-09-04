90f34cca1a74cbe6ff8eb17fc041a1d5899a764c
Luna Dilangalen <dadilangalen@gmail.com>
2020-09-04 10:50:04 +0800

    added report generation for threat_intel/squid

--- routes/threat_intel.js
+++ routes/threat_intel.js
@@ -76,6 +76,78 @@ router.post('/pihole', function (req, res, next) {
   });
 });
 
-//router.post('/squid')
+router.post('/squid', function (req, res, next) {
+  data = req.body;
+  console.log(data);
+  var reports = '';
+
+  // get datetime 
+  var dt = dateTime.create();
+  var formatted_date = dt.format('Y-m-d-H-M-S');
+  console.log('datetime: ' + formatted_date);
+  const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  // iterate through list of _source
+  const loop = async function() {
+    reports = reports + 'REPORT GENERATED AT: ' + formatted_date + "\n"
+    console.log(reports)
+    var report = ''
+    for (let i = 0; i < data.length; i++) {
+      // initialize report
+      report = ''
+
+      // parse data
+      var source_ip = data[i].source.ip;
+      var destination_ip = data[i].destination.ip;
+      var destination_domain = data[i].destination.domain;
+      var url_domain = data[i].url.domain;
+      var url_full = data[i].url.full;
+      // optional: encode url to base64
+      var b = new Buffer(url_full)
+      var url_b64 = (b.toString('base64')).replace(/=/g, '')
+
+      // report generation
+      report = report + 
+                   "--------------------------------------------------\n" +
+                   "source ip: " + source_ip + "\n" +
+                   "destination_ip: " + destination_ip + "\n" +
+                   "destination_domain: " + destination_domain + "\n" +
+                   "url_domain " + url_domain + "\n" + 
+                   "url_full: " + url_full + "\n" +
+                   "url_b64: " + url_b64 + "\n"
+
+      // // query threatcrowd API
+      // var response = await axios.get('https://www.threatcrowd.org/searchApi/v2/domain/report/?domain=' + dns_question_name)
+      // report = report + 
+      //          "threatcrowd_votes: " + response.data.votes + "\n"
+
+      // aggregate all reports
+      console.log("iteration "+i)
+      reports = reports + report
+      //await sleep(10)
+    }
+    return reports;
+  }
+
+  loop(data, reports)
+  .then(r => {
+    fs.appendFile("./reports" + "squid_" + formatted_date + ".txt", r, (err) => {
+      // throws an error, you could also catch it here
+      if (err) throw err;
+      // success case, the file was saved
+      console.log('Report saved!');
+    });
+    res.send('See report at http://10.150.0.7:3000/reports/'+ "squid_" + formatted_date + '.txt')
+  })
+  .catch(err => {
+    res.send(err)
+  });
+
+
+
+});
+
 
 module.exports = router
