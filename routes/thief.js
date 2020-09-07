f70cb0f05789f8a8c2e8ff6779fe19f0643599af
orangedeus <jpchanchix@live.com>
2020-09-07 19:27:31 +0800

    directory

--- routes/thief.js
+++ routes/thief.js
@@ -19,22 +19,23 @@ router.post('/check', function (req, res, next) {
   tar_commands = data.tar;
   curl_commands = data.curl;
 
+  var dt = dateTime.create();
+  var formatted_date = dt.format('Y-m-d-H-M-S');
+  var reports = 'REPORT GENERATED AT: ' + formatted_date + "\n"
   // regex patterns
+  const loop = async function() {
   var directory_pattern = /(\/)+[a-zA-Z0-9-_\/ ]*$/
   var compressed_file_pattern = /(\/)+[a-zA-Z0-9\-_\/ ]*(.tar.gz)/;
   var url_pattern = /(http|https):\/\/(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])(:[0-9]+)?/
-
   // get datetime 
-  var dt = dateTime.create();
-  var formatted_date = dt.format('Y-m-d-H-M-S');
   console.log('datetime: ' + formatted_date);
 
   // check for compressed files
-  var reports = 'REPORT GENERATED AT: ' + formatted_date + "\n"
   console.log(reports);
   console.log("iter\ttar compressed_file\tcurl_compressed_file\ttar host ip\tcurl host ip\tdestination_url");
   for (let i = 0; i < tar_commands.hits.length; i++) {
     // parse tar commands
+    tar_time = tar_commands.hits[i]._source["@timestamp"];
     tar_process_name = tar_commands.hits[i]._source.process.name;
     tar_process_title = tar_commands.hits[i]._source.process.title;
     tar_host_name = tar_commands.hits[i]._source.host.name;
@@ -47,16 +48,24 @@ router.post('/check', function (req, res, next) {
     
     for (let j = 0; j < curl_commands.hits.length; j++) {
       // parse curl commands
+      curl_time = curl_commands.hits[j]._source["@timestamp"];
       curl_process_name = curl_commands.hits[j]._source.process.name;
       curl_process_title = curl_commands.hits[j]._source.process.title;
       curl_host_name = curl_commands.hits[j]._source.host.name;
       curl_host_ipv4 = curl_commands.hits[j]._source.host.ip[0];
       curl_host_ipv6 = curl_commands.hits[j]._source.host.ip[1];
       curl_host_os = curl_commands.hits[j]._source.host.os.name;
+      curl_user_name = curl_commands.hits[j]._source.user.name;
 
       // search for any compressed files
-      var curl_compressed_file = curl_process_title.match(compressed_file_pattern)[0];
+      var curl_compressed_match = curl_process_title.match(compressed_file_pattern);
       // console.log(i + "\t" + tar_compressed_file + "\t" + curl_compressed_file + "\t" + tar_host_ipv4 + "\t" + curl_host_ipv4);
+
+      if (!curl_compressed_match) {
+        continue;
+      }
+      var curl_compressed_file = curl_compressed_match[0];
+
 
       if (tar_host_ipv4 == curl_host_ipv4) {
         // initialize singleton report
@@ -65,25 +74,79 @@ router.post('/check', function (req, res, next) {
         // extract the destination url
         var directory = tar_process_title.match(directory_pattern)[0];
         var destination_url = curl_process_title.match(url_pattern)[0];
-        console.log(i + "\t" + tar_compressed_file + "\t" + curl_compressed_file + "\t" + tar_host_ipv4 + "\t" + curl_host_ipv4 + "\t" + destination_url);
+
+        var dir_args = directory.split("/");
+        var dir_name = dir_args[dir_args.length - 1];
+
+        
+        var query = {
+  "from":0, "size":30,
+  "query": {
+    "bool": {
+      "must": [
+        {
+          "match": {
+            "event.module": "auditd"
+          }
+        },
+        {
+          "match": {
+            "event.action": "executed"
+          }
+        },
+        {
+          "match": {
+            "process.args": "mkdir"
+          }
+        },
+        {
+          "match": {
+            "process.args": dir_name
+          }
+        },
+        {
+          "range": {
+            "@timestamp": {
+              "gte": "now-15m",
+              "lt": "now"
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+        var response = await axios('http://10.150.0.6:9200/host-*/_search?', {method: "post",data: query});
+        var dir_time = response.data.hits.hits[0]._source["@timestamp"];
+        var dir_creation = response.data.hits.hits[0]._source.process.title;
+        console.log(dir_creation);
 
         // host name, host ip, host os, possibly created directory, possible exfiltrated file, possible exfiltration utilities, destination url
         report = report +
-                 "----------------------------------------------------------------\n"
+                 "----------------------------------------------------------------\n" +
                  "host name: " + curl_host_name + "\n" +
                  "host IP: " + curl_host_ipv4 + "\n" +
                  "host OS: " + curl_host_os + "\n" +
-                 "possible created directory: " + directory + "\n" +
+                 "user: " + curl_user_name + "\n" +
                  "possible exfiltrated file: " + curl_compressed_file + "\n" +
-                 "possible exfiltration utilities: " + curl_process_name + "\n" +
-                 "destination URL: " + destination_url + "\n"
-
+                 "destination URL: " + destination_url + "\n" +
+                 "curl timestamp: " + curl_time + "\n" +
+                 "curl command: " + curl_process_title + "\n" +
+                 "possible created directory: " + directory + "\n" +
+                 "possible directory creation date: " + dir_time + "\n" +
+                 "possible directory creation command: " + dir_creation + "\n" +
+                 "exfiltration utilities: " + curl_process_name + ", " + tar_process_name + ", " + response.data.hits.hits[0]._source.process.name + "\n" 
+         
         // concatenate singleton report to reports string
+        console.log("creating report");
         reports = reports + report;
       }
     }
   }
-  filename = "thief_" + formatted_date
+  return reports;
+  }
+  loop().then(r => {
+    filename = "thief_" + formatted_date
   fs.appendFile("./reports/" + filename + ".txt", reports, (err) => {
     // throws an error, you could also catch it here
     if (err) throw err;
@@ -91,6 +154,7 @@ router.post('/check', function (req, res, next) {
     console.log('Report saved!');
   });
   res.send('See report at http://10.150.0.7:3000/reports/'+ filename + '.txt')
+  });
 });
 
 module.exports = router
